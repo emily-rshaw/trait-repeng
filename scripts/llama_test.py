be756d1854d4e195b3553e858e9e983daa41c8e4
emiemory <156476276+emiemory@users.noreply.github.com>
2025-01-07 23:18:24 +0000

    Add llama test

---
+++ scripts/llama_test.py
@@ -0,0 +1,25 @@
+import os
+import torch
+from huggingface_hub import login
+from transformers import pipeline
+
+hf_token = os.environ.get("HUGGINGFACE_HUB_TOKEN", None)
+if hf_token:
+    login(token=hf_token)
+
+model_id = "meta-llama/Llama-3.2-3B-Instruct"
+pipe = pipeline(
+    "text-generation",
+    model=model_id,
+    torch_dtype=torch.bfloat16,
+    device_map="auto",
+)
+messages = [
+    {"role": "system", "content": "You are a pirate chatbot who always responds in pirate speak!"},
+    {"role": "user", "content": "Who are you?"},
+]
+outputs = pipe(
+    messages,
+    max_new_tokens=256,
+)
+print(outputs[0]["generated_text"][-1])
